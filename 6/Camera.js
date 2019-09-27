5e3e58dd66b037135138abab878706d44570487f
wzl <wzlh100@126.com>
2019-09-27 22:20:51 +0800

    lesson 6

--- 6/Camera.js
+++ 6/Camera.js
@@ -11,18 +11,128 @@ class Camera{
     }
 
     panX(v){
-        if(this.mode === Camera.MODE_ORBIT) return;
         this.updateViewMatrix();
         this.transform.position.x+=this.transform.right[0] * v;
+        if(this.mode === Camera.MODE_ORBIT) return;
         this.transform.position.y+=this.transform.right[1] * v;
         this.transform.position.z+=this.transform.right[2] * v;
     }
 
     panY(v){
-        if(this.mode === Camera.MODE_ORBIT) return;
         this.updateViewMatrix();
-        this.transform.position.x+=this.transform.up[0] * v;
         this.transform.position.y+=this.transform.up[1] * v;
+        if(this.mode === Camera.MODE_ORBIT) return;
+        this.transform.position.x+=this.transform.up[0] * v;
         this.transform.position.z+=this.transform.up[2] * v;
+    }
+
+    panZ(v){
+        this.updateViewMatrix();
+        if(this.mode === Camera.MODE_ORBIT){
+            this.transform.position.z+=v;
+        }else{
+            this.transform.position.x+=this.transform.up[0] * v;
+            this.transform.position.y+=this.transform.up[1] * v;
+            this.transform.position.z+=this.transform.up[2] * v;
+        }
+    }
+
+    updateViewMatrix(){
+        if(this.mode===Camera.MODE_FREE){
+            this.transform.matView.reset()
+                .vtranslate(this.transform.position)
+                .rotateX(this.transform.rotation.x * Transform.deg2Rad)
+                .rotateY(this.transform.rotation.y * Transform.deg2Rad);
+        }
+        else{
+            this.transform.matView.reset()
+                .rotateX(this.transform.rotation.x * Transform.deg2Rad)
+                .rotateY(this.transform.rotation.y * Transform.deg2Rad)
+                .vtranslate(this.transform.position);
+        }
+
+        this.transform.updateDirection();
+
+        Matrix4.invert(this.viewMatrix, this.transform.matView.raw);
+        return this.viewMatrix;
+    }
+}
+
+Camera.MODE_FREE = 0;
+Camera.MODE_ORBIT = 1;
+
+class CameraController{
+    constructor(gl,camera){
+        var oThis = this;
+        var box = gl.canvas.getBoundingClientRect();
+        this.canvas = gl.canvas;
+        this.camera = camera;
+
+        this.rotateRate = -300;
+        this.panRate = 5;
+        this.zoomRate = 200;
+
+        this.offsetX = box.left;
+        this.offsetY = box.top;
+
+        this.initX = 0;
+        this.initY = 0;
+        this.prevX = 0;
+        this.prevY = 0;
+
+        this.onUpHandler = function(e){
+            oThis.onMouseUp(e);
+        };
+        this.onMoveHandler = function(e){
+            oThis.onMouseMove(e);
+        };
+
+        this.canvas.addEventListener("mousedown",function(e){
+            oThis.onMouseDown(e);
+        });
+
+        this.canvas.addEventListener("mousewheel",function(e){
+            oThis.onMouseWheel(e);
+        });
+    }
+
+    getMouseVec2(e){
+        return {x:e.pageX-this.offsetX,y:e.pageY-this.offsetY};
+    }
+
+    onMouseDown(e){
+        this.initX = this.prevX = e.pageX - this.offsetX;
+        this.initY = this.prevY = e.pageY - this.offsetY;
+
+        this.canvas.addEventListener("mouseup",this.onUpHandler);
+        this.canvas.addEventListener("mousemove",this.onMoveHandler);
+    }
+
+    onMouseUp(e){
+        this.canvas.removeEventListener("mouseup",this.onUpHandler);
+        this.canvas.removeEventListener("mousemove",this.onMoveHandler);
+    }
+
+    onMouseWheel(e){
+        var delta = Math.max(-1,Math.min(1,(e.wheelDelta || -e.detail)));
+        this.camera.panZ(-delta * (this.zoomRate / this.canvas.height));
+    }
+
+    onMouseMove(e){
+        var x = e.pageX - this.offsetX,
+            y = e.pageY - this.offsetY,
+            dx = x - this.prevX,
+            dy = y - this.prevY;
+
+        if(e.shiftKey){
+            this.camera.transform.rotation.y += dx * (this.rotateRate / this.canvas.width);
+            this.camera.transform.rotation.x += dy * (this.rotateRate / this.canvas.height);
+        }else{
+            this.camera.panX(-dx * (this.panRate/this.canvas.width));
+            this.camera.panY(dy * (this.panRate/this.canvas.height));
+        }
+
+        this.prevX = x;
+        this.prevY = y;
     }
 }
